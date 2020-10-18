2f28f1d8e1274d3f4ed4c6a5e015b178dc431a1b
Shades <mydude@shadess-mbp.myfiosgateway.com>
2020-10-18 13:19:00 -0400

    Landing Page moved to React component

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,28 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import LandingPage from './LandingPage'
+
 
 export const App = (props) => {
   return (
-    <div className='body'>
-      <h2 className='center-text'>
-        Hello and Welcome!
-      </h2>
-      <img
-        src="/assets/shades.png"
-        alt='Patrick "Shades", sitting in a car, smiling'
-        className="center"
-      />
-      <div className="textbox">
-        <p>
-          My name is Patrick Wetzel-Meyers, but having grown up in a town and school system with many other Patricks my own age, I prefer to go by "Shades". I am a full-stack developer in the JavaScript and Ruby languages.
-        </p>
-        <p>
-          I started my coding journey with Learn Ruby the Hard Way, before signing up for Launch Academy's Coding Bootcamp.
-        </p>
-        <p>
-          Launch Academy's Coding Bootcamp course was a wonderful learning experience for me. I learned more about Ruby, I learned JavaScript, and I learned both the Rails and React frameworks. I also learned about Object Oriented Programming, and Pair Programming. Most of this was learned while working remotely, due to the COVID pandemic shutting down in-person classes after only one week together. Most importantly, Launch taught me how to think about code, not just how to write it.
-        </p>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={LandingPage}
+      </Switch>
+    </BrowserRouter>
   )
 }
 
